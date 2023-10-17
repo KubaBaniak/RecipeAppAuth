91d7723d96ef6d7f35a22d46e661ce0b00be059b
Jakub Urbaniak <kuba121201@gmail.com>
2023-10-17 12:07:49 +0200

    chore: commit missing files

---
+++ src/auth/__mocks__/pending-user-credentials.repository.mock.ts
@@ -0,0 +1,30 @@
+import { PendingUserCredentialsRepository } from '../repositories';
+import { PendingUserCredentials } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+export class MockPendingUserCredentialsRepository extends PendingUserCredentialsRepository {
+  storePendingUserCredentials(
+    userId: number,
+    password: string,
+  ): Promise<PendingUserCredentials> {
+    return Promise.resolve({ userId, password });
+  }
+
+  getPendingUserCredentialsById(
+    userId: number,
+  ): Promise<PendingUserCredentials | null> {
+    return Promise.resolve({
+      userId,
+      password: faker.internet.password({ length: 64 }),
+    });
+  }
+
+  removePendingUserCredentialsById(
+    userId: number,
+  ): Promise<PendingUserCredentials> {
+    return Promise.resolve({
+      userId,
+      password: faker.internet.password({ length: 64 }),
+    });
+  }
+}
